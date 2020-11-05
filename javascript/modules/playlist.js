775959fad1e95e51f5fad56f67975e4fdf80c77a
Adam Hauze <hauze_v@protonmail.com>
2020-11-05 16:39:28 -0500

    Finished the rest of the render function logic which uses some ternary operators to determine if the song is selected or paused or not and thus displays that list item appropriately.

--- javascript/modules/playlist.js
+++ javascript/modules/playlist.js
@@ -2,9 +2,9 @@ import { songsList } from '../data/songs.js'; // grabs the songlist array object
 
 /* Use the revealing module pattern*/
 const Playlist = (() => {
-  /* Data or State */
+  /* Data and State Variables */
   let songs = songsList;
-  let currentlyPlayingIndex = 0;
+  let currentlyPlayingIndex = 4;
   // We're using a built-in web API from the browser to play audio and get various audio file information
   let currentSong = new Audio(songs[currentlyPlayingIndex].url);
   let isPlaying = false;
@@ -19,11 +19,24 @@ const Playlist = (() => {
   const render = () => {
     let markup = '';
 
+    const togglePlayPauseIcon = (itemIndex) => {
+      /* If current index matches index argument, check if song is paused and return icon based on its' state 
+         If the current index does not match, simply return the play button. */
+      if (currentlyPlayingIndex === itemIndex) {
+        return currentSong.paused ? "fa-play" : "fa-pause";
+      } else {
+        return "fa-play";
+      }
+    }
+
+    /* Loop through the songs and render each one appropriately */
     songs.forEach((songObj, index) => {
+      /* We use two JavaScript ternary operators right off the bat here to check if the index of the forEach loop is equal to the currentlyPlayingIndex
+         If it is, we add an additional CSS class that will style that list item differently. */
       markup += `
-        <li class="playlist__song">
+        <li class="playlist__song ${index === currentlyPlayingIndex ? "playlist__song--active" : ""}">
           <div class="play-pause">
-            <i class="fa fa-play pp-icon"></i>
+            <i class="fa ${togglePlayPauseIcon(index)} pp-icon"></i>
           </div>
           <div class="playlist__song-details">
             <span class="playlist__song-name">${songObj.title}</span>
